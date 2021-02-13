708d3f700294f545278e0f8c2b56084409bab099
Nate Ni <natengai.nn@gmail.com>
2021-02-13 23:42:47 +0800

    Updated second version

--- password.py
+++ password.py
@@ -1,6 +1,6 @@
 correct = 'a123456'
 i = 3
-while True:
+while i > 0:
     pw = input('請輸入密碼： ')
     if pw == correct:
         print('登入成功')
@@ -8,5 +8,3 @@ while True:
     else:
         i -= 1
         print('密碼錯誤！還有',i,' 次機會')
-        if i == 0:
-            break
